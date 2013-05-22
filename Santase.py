a4a43ad172b86fef7cf68b64b474c132220b0373
blacktoli <tolidtm@gmail.com>
2013-05-23 00:25:19 +0200

    Changing the structure

--- Santase.py
+++ Santase.py
@@ -4,44 +4,40 @@ from os import system
 
 
 def all_cards():
-	cards = ["9","10","J","Q","K","A"]
-	color = ["d","c","h","s"]
-	CARDS = []
-	for card in cards:
-		for one in color:
-			CARDS.append(card + one)
-	shuffle(CARDS)
-	return CARDS
+    """
+    The cards, return shuffle cards
+    """
+    titles = ["9", "10", "J", "Q", "K", "A"]
+    colors = ["d", "c", "h", "s"]
+    cards = []
+    for title in titles:
+        for color in colors:
+            cards.append(title + color)
+    shuffle(cards)
+    return cards
 
 
-def card_distribution(p1,p2):
-	if len(p1["cards"]) > 6 or len(p2["cards"]) > 6:
-		print "You cheater, some one have more than 6 cards"
-	if len(Cards) > 0:
-		while len(p1["cards"]) < 6 or len(p2["cards"]) < 6:
-			if len(p1["cards"]) < 6:
-				p1["cards"].append(Cards.pop(-1))
-			if len(p2["cards"]) < 6:
-				p2["cards"].append(Cards.pop(-1))
-
-
-def card_trumb():
-	Cards.insert(0,Cards.pop(-1))
-
-
-def card_withdraw(player):
-	player.append(Cards.pop(-1))
+def card_distribution(p1, p2):
+    """
+    Distributes cards to players
+    """
+    while len(p1["cards"]) < 6 or len(p2["cards"]) < 6:
+        if len(p1["cards"]) < 6:
+            p1["cards"].append(Cards.pop(-1))
+        if len(p2["cards"]) < 6:
+            p2["cards"].append(Cards.pop(-1))
 
 
 def card_score(card):
-	''' Card type example "Kh"'''
-	scores = {"9":0,"10":1,"J":2,"Q":3,"K":4,"A":5}
-	score = scores[card[:-1]]
-	if card[-1] == trumb[-1][-1]:
-		return score + 10
-	else: return score
-
-
+    ''' Card type example "Kh", return score. if the card is trumb the cards is bigger then the others '''
+    scores = {"9": 0, "10": 1, "J": 2, "Q": 3, "K": 4, "A": 5}
+    score = scores[card[:-1]]
+    if card[-1] == trumb[-1][-1]:
+        return score + 10
+    else:
+        return score
+    
+    
 def card_given(player, card):
     while True:
         try:
@@ -54,45 +50,34 @@ def card_given(player, card):
             return False
 
 
-def New_cards():
-    global Cards
-    Cards = []
-    return all_cards()
+def card_withdraw(player):
+    player.append(Cards.pop(-1))
+
 
 def input(msg):
     return raw_input(msg + ">>>> ::: ")
 
-def player_name():
-    global player1
-    global player2
-    print """Starting new game
-    \n\n\nPlease enter players name"""
-    player1["name"] = input("Player 1 name: ")
-    player2["name"] = input("Player 2 name: ")
 
-
-def gui():
+def menu(game = 0):
     system('clear')
-    print "\t\t\tWelcome to the Santase Card Game\n\n\n"
-    print """\t%s
-        \nCards: %s\nHands: %s\nScore: %s\t Card given: %s
-        \n\n\t%s
-        \nCards: %s\nHands: %s\nScore: %s\tCard given: %s""" % (player1["name"], ' '.join(player1["cards"]), ' '.join(player1["hands"]), player1["score"], player1["given"], player2["name"], ' '.join(player2["cards"]), ' '.join(player2["hands"]), player2["score"], player2["given"])
-    print "\nTRUMB: %s\nCards left: %s\t%s" % ("".join(trumb), len(Cards), "#" * (len(Cards)))
-    menu_choice()
+    if game:
+        print "\t\t\tWelcome to the Santase Card Game\n\n\n"
+        print """\t%s
+            \nCards: %s\nHands: %s\nScore: %s\t Card given: %s
+            \n\n\t%s
+            \nCards: %s\nHands: %s\nScore: %s\tCard given: %s""" % (player1["name"], ' '.join(player1["cards"]), ' '.join(player1["hands"]), player1["score"], player1["given"], player2["name"], ' '.join(player2["cards"]), ' '.join(player2["hands"]), player2["score"], player2["given"])
+        print "\nTRUMB: %s\nCards left: %s\t%s" % ("".join(trumb), len(Cards), "#" * (len(Cards)))
+    print """\n\nMENU\n\n(New) - Will start new game\n\n(S)tart - will start the game(Exit)"""
 
-def menu_choice():
-    print "\n\nMENU\n\n(New)\n(Exit)"
 
-def start_game():
-    global Cards
-    game()
 def turn_change(player):
     player["turn"] = 1
+
 
 def hand_append(player, card1, card2):
     player["hands"].append(card1)
     player["hands"].append(card2)
+
 
 def check(player1_score, player2_score):
 
@@ -109,8 +94,11 @@ def check(player1_score, player2_score):
         gui()
         print "%s win the hand" % player2["name"]
 
+
 def whose_turn(): #TODO: help
     while len(player1["cards"]) > 0 or len(player2["cards"]) > 0:
+        
+        Cards.insert(0, Cards.pop(-1)) # To implement with if statement
         if player1["turn"] == 1:
             player1_score = player_turn(player1)
             player2_score = player_turn(player2)
@@ -121,45 +109,41 @@ def whose_turn(): #TODO: help
             check(player1_score, player2_score)
 
 
-def player_turn(player):
-    while True:#TODO: help 2 :)
+def game(player):
+    while True:  # TODO: help 2 :)
         if card_given(player, input(player["name"] + "'s turn")):
             print "\n\nCard given by %s: %s\n\n" %(player["name"], player["given"])
             gui()
             return player["given"]
 
 
-
-def game():
+def main():
     Cards = []
-    player1 = {"name": "", 'cards': [], 'turn': 1, 'given': 0, 'score': 0, 'hands':[], "totalscore": 0}
-    player2 = {"name": "", 'cards': [], 'turn': 0, 'given': 0, 'score': 0, 'hands':[], "totalscore": 0}
+    player1 = {"name": "", 'cards': [], 'turn': 1, 'given': 0, 'score': 0, 'hands':
+    [], "totalscore": 0}
+    player2 = {"name": "", 'cards': [], 'turn': 0, 'given': 0, 'score': 0, 'hands':
+        [], "totalscore": 0}
     color = ""
-
-    menu_choice()
     while True:
-        choice = input("")
-        if choice == "new" or choice == "NEW":
-            player_name()
-            choice = "s"
-        if choice == "s": # Starting new game clean all cards
-            Cards = New_cards()
-            card_distribution(player1, player2)
-            card_trumb()
+        menu()
+        
+        if choice == "s" or choice == "S":
+            if not player1["name"] or not player2["name"]:
+                player1["name"] = raw_input("Please type player 1 name: ")
+                player2["name"] = raw_input("Please type player 2 name: ")
+            Cards = all_cards()
             trumb = Cards[0]
             gui()
-            whose_turn()
-        if choice == "exit" or choice == "EXIT":
+            game()
+        elif player1["name"] or player2["name"]:
+            print "(C)ontinue the game!"
+        elif choice == "c" or choice == "C":
+            print "ok"
+        elif choice == "exit" or choice == "EXIT":
             exit()
+        choice = input("")
+
+if __name__ == "__main__":
+    main()
 
 
-
-start_game()
-
-
-##while True:
-##            system('clear')
-##            gui()
-##        else: input(player2["name"] + "'s turn")
-##        input(" ")
-##
